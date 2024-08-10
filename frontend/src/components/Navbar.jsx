7769e444b58f4ca2e89fdd4721a05d88e4590585
RoniaZhang <XiaQian1999@gmail.com>
2024-08-10 11:42:26 -0400

    Frontend: Design for navbar and sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center p-2 h-[120px]'>
+        <img src={assets.logo} alt="logo" className='w-[220px]' />
+        <img src={assets.profile_image} alt="profile image" className='w-[50px]' />
+    </div>
   )
 }
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
+import { assets } from '../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='w-[18%] border border-gray-400 border-t-0 font-semibold' style={{minHeight: '100vh'}}>
+        <div className='pt-5 pl-[20%] flex flex-col gap-4'>
+            <NavLink to={"/addjob"} className="flex items-center gap-4 border border-gray-400 border-r-0 py-5 px-3 rounded-l-md cursor-pointer">
+                <p className='hidden sm:block'>Add Job</p>
+                <img src={assets.add_icon} alt="add icon" className='w-8' />
+            </NavLink>
+
+            <NavLink to={"/listjob"} className="flex items-center gap-4 border border-gray-400 border-r-0 py-5 px-3 rounded-l-md cursor-pointer">
+                <p className='hidden sm:block'>List Job</p>
+                <img src={assets.list_icon} alt="list icon" className='w-8' />
+            </NavLink>
+
+            <NavLink to={"/viewcalendar"} className="flex items-center gap-4 border border-gray-400 border-r-0 py-5 px-3 rounded-l-md cursor-pointer">
+                <p className='hidden sm:block'>View Calendar</p>
+                <img src={assets.calendar_icon} alt="calendar icon" className='w-8' />
+            </NavLink>
+        </div>
+    </div>
   )
 }
 
