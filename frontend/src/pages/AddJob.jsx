d4bf3b289b7c17372d742023f70016c6749617cc
RoniaZhang <XiaQian1999@gmail.com>
2024-08-10 22:06:44 -0400

    Create onchangeHandler function for the form

--- frontend/src/pages/AddJob.jsx
+++ frontend/src/pages/AddJob.jsx
@@ -1,60 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const AddJob = () => {
-  
+  const [data, setData] = useState({
+    title: "",
+    application_url: "",
+    required_skill: "",
+    optional_skill: "",
+    salary_range: "",
+    year_of_experience: "",
+    locations: "",
+    additional_note: "",
+    rate_interest: ""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({...data, [name]:value}))
+  }
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return (
     <div className='w-[70%] ml-5 mt-5 text-gray-600'>
       <form action="" className='flex flex-col w-full'>
 
         <div className='flex flex-col mb-4'>
           <p>Title*</p>
-          <input type="text" name="title" className='p-3 border border-gray-400 rounded-md' required/>
+          <input onChange={onChangeHandler} value={data.title} type="text" name="title" className='p-3 border border-gray-400 rounded-md' required/>
         </div>
 
         <div className='flex flex-col mb-4'>
           <p>Application Link*</p>
-          <input type="url" name="application_url" className='p-3 border border-gray-400 rounded-md' required />
+          <input onChange={onChangeHandler} value={data.application_url} type="url" name="application_url" className='p-3 border border-gray-400 rounded-md' required />
         </div>
 
         <div className='flex flex-col mb-4'>
           <p>Required Skill*</p>
-          <select name="required_skill" className='p-3 border border-gray-400 rounded-md' required>
+          <select onChange={onChangeHandler} name="required_skill" className='p-3 border border-gray-400 rounded-md' required>
             <option value=""></option>
           </select>
         </div>
 
         <div className='flex flex-col mb-4'>
           <p>Optional Skill</p>
-          <input type="text" name="optional_skill" className='p-3 border border-gray-400 rounded-md' />
+          <input onChange={onChangeHandler} value={data.optional_skill} type="text" name="optional_skill" className='p-3 border border-gray-400 rounded-md' />
         </div>
 
         <div className='flex flex-wrap justify-between my-2'>
           <div className='flex flex-col mb-4' style={{width: "48%"}}>
             <p>Salary Range</p>
-            <input type="number" name="salary_range" className='p-3 border border-gray-400 rounded-md' />
+            <input onChange={onChangeHandler} value={data.salary_range} type="number" name="salary_range" className='p-3 border border-gray-400 rounded-md' />
           </div>
 
           <div className='flex flex-col mb-4' style={{width: "48%"}}>
             <p>Year of Experience</p>
-            <input type="text" name="year_of_experience" className='p-3 border border-gray-400 rounded-md' />
+            <input onChange={onChangeHandler} value={data.year_of_experience} type="text" name="year_of_experience" className='p-3 border border-gray-400 rounded-md' />
           </div>
         </div>
 
         <div className='flex flex-wrap justify-between my-2'>
           <div className='flex flex-col mb-4' style={{width: "48%"}}>
             <p>Locations</p>
-            <input type="text" name="locations" className='p-3 border border-gray-400 rounded-md' />
+            <input onChange={onChangeHandler} value={data.locations} type="text" name="locations" className='p-3 border border-gray-400 rounded-md' />
           </div>
 
           <div className='flex flex-col mb-4' style={{width: "48%"}}>
             <p>Rate of Interest</p>
-            <input type="text" name="rate_interest" className='p-3 border border-gray-400 rounded-md' />
+            <input onChange={onChangeHandler} value={data.rate_interest} type="text" name="rate_interest" className='p-3 border border-gray-400 rounded-md' />
           </div>
         </div>
 
         <div className='flex flex-col mb-4'>
           <p>Additional Note</p>
-          <textarea type="text" name="additional_note" className='p-3 border border-gray-400 rounded-md'>
+          <textarea onChange={onChangeHandler} value={data.additional_note} type="text" name="additional_note" className='p-3 border border-gray-400 rounded-md'>
           </textarea>
         </div>
 
