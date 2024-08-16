e2be2ff3b1c19271e4435d18e7783bc1e38ecf8a
RoniaZhang <XiaQian1999@gmail.com>
2024-08-15 21:59:05 -0400

    Backend: User able to submit the form to MongoDB with selected items

--- frontend/src/pages/AddJob.jsx
+++ frontend/src/pages/AddJob.jsx
@@ -12,15 +12,16 @@ const AddJob = () => {
     application_url: "",
     required_skill: "",
     optional_skill: "",
-    salary_range: "",
+    salary_range: 0,
     year_of_experience: "",
     locations: "",
     additional_note: "",
-    rate_interest: ""
+    rate_interest: 0
   })
 
   const [skills, setSkills] = useState([]);
   const [selectedSkills, setSelectedSkills] = useState([]);
+  // console.log( selectedSkills.map(skill => skill.value));
 
   const onChangeHandler = (event) => {
     const name = event.target.name;
@@ -28,10 +29,59 @@ const AddJob = () => {
     setData(data => ({...data, [name]:value}))
   }
 
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    //In this case we don't use FormData, where the data is send as "multipart/form-data", it's different from JSON
+    //Therefore only need to change here for the postData, no backend change needed
+
+    const postData = {
+      title: data.title,
+      application_url: data.application_url,
+      required_skill: selectedSkills.map(skill => skill.value),
+      optional_skill: data.optional_skill,
+      salary_range: data.salary_range,
+      year_of_experience: data.year_of_experience,
+      locations: data.locations,
+      additional_note: data.additional_note,
+      rate_interest: data.rate_interest
+    }
+
+    try {
+      const response = await axios.post(`${url}/api/post/add`, postData, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (response.data.success){
+          // if success, reset the form
+          setData({
+            title: "",
+            application_url: "",
+            required_skill: [],
+            optional_skill: "",
+            salary_range: 0,
+            year_of_experience: "",
+            locations: "",
+            additional_note: "",
+            rate_interest: 0
+          })
+    
+          toast.success(response.data.message);
+        }else{
+          console.log("ERROR: ", response.data.message);
+        }
+
+    } catch (error) {
+      console.log("Error submitting form: ", error);
+    }
+  }
+
   const fetchSkills = async () => {
     try {
       const response = await axios.get(`${url}/api/skill/skills`);
-      console.log(response.data);
+      // console.log(response.data);
 
       const skillData = response.data.data.map(skill => ({
         value: skill.value,
@@ -46,12 +96,12 @@ const AddJob = () => {
 
   useEffect(() => {
     fetchSkills();
+    console.log(data)
   }, [])
 
   return (
     <div className='w-[70%] ml-5 mt-5 text-gray-600'>
-      <form action="" className='flex flex-col w-full'>
-
+      <form className='flex flex-col w-full' onSubmit={onSubmitHandler}>
         <div className='flex flex-col mb-4'>
           <p>Title*</p>
           <input onChange={onChangeHandler} value={data.title} type="text" name="title" className='p-3 border border-gray-400 rounded-md' required/>
@@ -64,7 +114,12 @@ const AddJob = () => {
 
         <div className='flex flex-col mb-4'>
           <p>Required Skill*</p>
-          <Select isMulti options={skills} value={selectedSkills} onChange={setSelectedSkills} placeholder="Select or add skill" />
+          <Select 
+            isMulti 
+            options={skills} 
+            value={selectedSkills} 
+            onChange={setSelectedSkills} 
+            placeholder="Select or add skill" />
         </div>
 
         <div className='flex flex-col mb-4'>
