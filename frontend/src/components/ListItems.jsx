f187265a9fe648fbdddef0ebc13d4ce826d253e8
RoniaZhang <XiaQian1999@gmail.com>
2024-08-17 21:15:12 -0400

    Frontend: minor update on design

--- frontend/src/components/ListItems.jsx
+++ frontend/src/components/ListItems.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const ListItems = ({id, title, application_url, required_skill, optional_skill, salary_range, year_of_experience, locations, rate_interest, additional_note, removeList}) => {
   return (
-    <div id={id} className='w-full h-auto m-auto rounded-xl shadow-lg' style={{ transition: '0.3s', animation: 'fadeIn 1s'}}>
+    <div id={id} className='w-full h-auto m-auto rounded-xl shadow-lg flex flex-col' style={{ transition: '0.3s', animation: 'fadeIn 1s'}}>
+        <div className='py-3 bg-green-800 text-white text-center rounded-t-xl text-lg cursor-pointer'>
+            <a href={application_url} target="_blank">{title}</a>
+        </div>
         <div>
-            <div className='py-3 bg-green-800 text-white text-center rounded-t-xl text-lg cursor-pointer'>
-                <a href={application_url} target="_blank">{title}</a>
-            </div>
-            <div className='p-2'>
+            <div className='p-2 break-words'>
                 {required_skill && required_skill.length > 0 ?
                 (
                     required_skill.map((skill, skillIndex) => (
-                        <span key={skillIndex} className='mx-1 px-3 py-1 bg-gray-700 rounded-full text-white text-sm'>{skill}</span>
+                        <span key={skillIndex} className='mx-1 px-3 py-1 bg-gray-700 rounded-full text-white text-sm whitespace-nowrap leading-8'>{skill}</span>
                     ))
                 ) : (
                     <span>No Required Skills</span>
@@ -38,11 +38,11 @@ const ListItems = ({id, title, application_url, required_skill, optional_skill, 
                 <div className='p-2'>Addition Notes: <span className='text-red-500'> {additional_note} </span> </div> : ""
             }
 
-            <div className='p-2'>
+            <div className='p-2 break-words'>
                 {optional_skill && optional_skill.length > 0 ?
                 (
                     optional_skill.map((skill, skillIndex) => (
-                        <span key={skillIndex} className='mx-1 px-3 py-1 bg-gray-300 rounded-full text-white text-sm'>{skill}</span>
+                        <span key={skillIndex} className='mx-1 px-3 py-1 bg-gray-300 rounded-full text-white text-sm whitespace-nowrap leading-8'>{skill}</span>
                     ))
                 ):(
                     ""
@@ -50,8 +50,8 @@ const ListItems = ({id, title, application_url, required_skill, optional_skill, 
             }
             </div>
         </div>
-        
-        <button onClick={()=>removeList(id)} className='py-3 bg-red-400 hover:bg-red-500 text-white text-center rounded-b-xl text-lg cursor-pointer w-full'>Remove</button>
+
+        <button onClick={()=>removeList(id)} className='py-1 px-2 bg-red-400 hover:bg-red-500 text-white text-center cursor-pointer w-fit place-self-end mr-2 mb-2 rounded-md text-sm'>Remove</button>
     </div>
   )
 }
