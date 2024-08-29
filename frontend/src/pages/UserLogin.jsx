0af59fd64be17db5ef9a5b9604be2d241c70a2ea
RoniaZhang <XiaQian1999@gmail.com>
2024-08-28 21:27:19 -0400

    Frontend: User able to switch between login/register while initialize backend

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,35 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UserLogin = (url, setIsAuthenticated) => {
   
-  const [currState, setCurrState] = useState("Sign In")
+  const [currState, setCurrState] = useState("Login")
+  const [data, setData] = useState({
+    name:"",
+    email: "",
+    password: ""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data, [name]:value}))
+  }
+
+
   return (
     <div className='w-full h-screen flex flex-row z-1 text-white py-10 justify-center'>
       <div className='bg-green-800 py-5 px-10 rounded self-center text-center'>
-        <form className='grid grid-flow-row gap-4'>
+        <form className='grid grid-flow-row gap-4 '>
           <div className='grid grid-flow-row gap-4'>
             <h1 className='text-[40px]'>{currState}</h1>
             <p>Free access to our dashboard.</p>
-            <button className='border border-white px-3 py-2 rounded cursor-pointer'>Sign in with Google</button>
+            <button className='border border-white px-3 py-2 rounded cursor-pointer'>Login with Google</button>
           </div>
 
           <hr className='my-5'/>
           {currState==="Sign Up" ? 
             <div className='grid'>
               <p className='text-left'>Full Name</p>
-              <input type="text" className='border border-gray-500 px-3 py-2 rounded' />
+              <input name="name" onChange={onChangeHandler} value={data.name} type="text" className='border border-gray-500 px-3 py-2 rounded text-black' />
             </div>
           : <></>
           }
 
           <div className='grid'>
             <p className='text-left'>Email Address</p>
-            <input type="text" className='border border-gray-500 px-3 py-2 rounded' placeholder='name@example.com' />
+            <input name="email" onChange={onChangeHandler} value={data.email} type="text" className='border border-gray-500 px-3 py-2 rounded text-black' placeholder='name@example.com' />
           </div>
 
           <div className='grid'>
           <p className='text-left'> Password </p>
-            <input type="text" className='border border-gray-500 px-3 py-2 rounded' />
+            <input name="password" onChange={onChangeHandler} value={data.password} type="password" className='border border-gray-500 px-3 py-2 rounded text-black' />
           </div>
 
           {currState==="Sign Up"
@@ -37,11 +50,11 @@ const UserLogin = (url, setIsAuthenticated) => {
             : <div className='text-left'><input type="checkbox" /> Remember me</div>
           }
 
-          <button className='border border-white px-3 py-2 rounded cursor-pointer'>{currState==="Sign Up" ? "Sign Up" : "Sign In"}</button>
+          <button className='border border-white px-3 py-2 rounded cursor-pointer'>{currState==="Sign Up" ? "Sign Up" : "Login"}</button>
 
           {currState==="Sign Up"
-            ? <p>Already have an account? <a href="">Sign In here</a></p>
-            : <p>Don't have an account yet? <a href="">Sign up here</a></p>
+            ? <p>Already have an account? <span className='cursor-pointer text-black hover:text-orange-600' onClick={()=> setCurrState("Login")}>Login here</span></p>
+            : <p>Don't have an account yet? <span className='cursor-pointer text-black hover:text-orange-600' onClick={()=> setCurrState("Sign Up")}>Sign up here</span></p>
           }
         </form>
       </div>
