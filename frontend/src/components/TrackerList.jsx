94d7ea35c04504030c75840fc0c298f59c141fd1
RoniaZhang <XiaQian1999@gmail.com>
2024-08-31 11:52:45 -0400

    Fullstack: User able to add the list under tracker

--- frontend/src/components/TrackerList.jsx
+++ frontend/src/components/TrackerList.jsx
@@ -4,6 +4,44 @@ import { toast } from 'react-toastify';
 
 const TrackerList = ({ title, port_url }) => {
   const [list, setList] = useState([]);
+  const [data, setData] = useState({
+    task: "",
+    tracker_type: "",
+    status:1
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({...data, [name]:value}));
+  }
+
+  const onSubmitHanlder = async (event) => {
+    event.preventDefault();
+    const trackerListData = {
+      task: data.task,
+      tracker_type: title === "Daily" ? "daily" : "annual",
+      status: 1
+    }
+
+    try {
+      const response = await axios.post(`${port_url}/api/trackerlist/add`, trackerListData, {
+        headers: {'Content-Type': 'application/json'}
+      });
+
+      if(response.data.success){
+        setData({
+          task: "",
+          tracker_type: "",
+          status:1
+        })
+      }else{
+        console.log("ERROR: ", response.data.message);
+      }
+    } catch (error) {
+      console.log("Error submitting form: ", error);
+    }
+  }
 
   const fetchList = async () => {
     try {
@@ -25,12 +63,8 @@ const TrackerList = ({ title, port_url }) => {
   return (
     <div className='border border-gray-200 flex flex-auto flex-col rounded p-4 bg-white'>
         <p className='font-bold text-[20px] self-center ml-2'>{title} Tracker</p>
-        <form className='flex flex-wrap justify-between my-2'>
-          <input name="" type="text" className='flex-1 border border-gray-400 p-2 rounded' />
-          {title === "Daily" 
-            ? <input type="text" hidden />
-            : <input type="text" hidden />
-          }
+        <form className='flex flex-wrap justify-between my-2' onSubmit={onSubmitHanlder}>
+          <input onChange={onChangeHandler} name="task" value={data.task} type="text" className='flex-1 border border-gray-400 p-2 rounded' />
           <button type="submit" className='text-white bg-gray-600 hover:bg-gray-700 cursor-pointer px-3 py-2 rounded mx-1'>Add</button>
         </form>
 
