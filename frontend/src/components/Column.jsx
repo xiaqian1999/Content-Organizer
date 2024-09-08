0dd9381eb65eb62fb7c30ec17f716cc644c63dbc
RoniaZhang <XiaQian1999@gmail.com>
2024-09-07 22:55:58 -0400

    Frontend: Visualize design update when drag and drop

--- frontend/src/components/Column.jsx
+++ frontend/src/components/Column.jsx
@@ -4,18 +4,24 @@ import Task from './Task';
 
 const Column = ({column, tasks}) => {
   return (
-    <div className='border border-gray-400 rounded-md p-2'>
-        <h2 className='font-bold'>{column.title}</h2>
+    <div className='border border-gray-400 rounded-md bg-white'>
+        <h2 className='font-bold text-center'>{column.title}</h2>
         <Droppable droppableId={column.id}>
-            {(provided) =>(
-                <div ref={provided.innerRef}
-                    {...provided.droppableProps}>
-                    {tasks.map((task, index) => { 
-                        return (<Task key={task.id} task={task} index={index} />)
-                    })}
-                    {provided.placeholder}
-                </div>
-            )}
+            {(provided, snapshot) =>{
+                const backgroundColor = snapshot.isDraggingOver ? 'bg-gray-200' : 'bg-white';
+                return(
+                    <div ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        className={`${backgroundColor} p-4`}
+                        style={{transition: 'background-color 0.2s ease'}}
+                    >
+                        {tasks.map((task, index) => { 
+                            return (<Task key={task.id} task={task} index={index} />)
+                        })}
+                        {provided.placeholder}
+                    </div>
+                )
+            }}
         </Droppable>
     </div>
   )
--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 import { Draggable } from '@hello-pangea/dnd';
 
 const Task = ({task, index}) => {
+    const draggableSnapShot = {
+        isDragging: true,
+        draggingOver: 'column-1',
+    }
+
     return (
         <Draggable draggableId={task.id} index={index}>
-            {(provided) => (
-                <div {...provided.draggableProps}
+            {/*snapshot - contains # of property to style draggable components during drag */}
+            {(provided, snapshot) => {
+                //Another approach to do the same thing by determine the bg color based on dragging state
+                // const backgroundColor = snapshot.isDragging ? 'bg-green-200' : 'bg-white';
+                return (
+                    <div {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref = {provided.innerRef}
-                    className='border border-black rounded-md p-2 mb-2 bg-white'>
+                    // isDragging={snapshot.isDragging}
+                    // We dont use the above line due to that tailwind css doesn't support dynamic css properties
+                    // Need to handle dynamic class names within the component logic
+                    className={`border border-black rounded-md p-2 mb-2 ${snapshot.isDragging ? 'bg-green-200' : 'bg-white'}`}>
                     {task.content}
                 </div>
-            )}
+                )
+            }}
         </Draggable>
     )
 }
