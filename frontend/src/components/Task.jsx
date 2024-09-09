5c82fd5f63b7898810d9e4e56c856a99635b8f3f
RoniaZhang <XiaQian1999@gmail.com>
2024-09-08 23:00:41 -0400

    Fullstack: Disable a task to be draggable

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -7,8 +7,14 @@ const Task = ({task, index}) => {
         draggingOver: 'column-1',
     }
 
+    const isDragDisabled = task.id === 'task-1';
+
     return (
-        <Draggable draggableId={task.id} index={index}>
+        <Draggable 
+            draggableId={task.id} 
+            index={index}
+            isDragDisabled={isDragDisabled}
+            >
             {/*snapshot - contains # of property to style draggable components during drag */}
             {(provided, snapshot) => {
                 //Another approach to do the same thing by determine the bg color based on dragging state
@@ -20,7 +26,7 @@ const Task = ({task, index}) => {
                     // isDragging={snapshot.isDragging}
                     // We dont use the above line due to that tailwind css doesn't support dynamic css properties
                     // Need to handle dynamic class names within the component logic
-                    className={`border border-black rounded-md p-2 mb-2 ${snapshot.isDragging ? 'bg-green-200' : 'bg-white'}`}>
+                    className={`border border-black rounded-md p-2 mb-2 ${isDragDisabled ? 'bg-gray-400' : ''} ${snapshot.isDragging ? 'bg-green-200' : 'bg-white'}`}>
                     {task.content}
                 </div>
                 )
