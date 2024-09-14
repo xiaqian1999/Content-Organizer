5670523eb4ba13c826b4f9e823ee0950eca6d580
RoniaZhang <XiaQian1999@gmail.com>
2024-09-14 11:24:58 -0400

    Frontend: Update design for drag item

--- frontend/src/components/TrackerItems.jsx
+++ frontend/src/components/TrackerItems.jsx
@@ -11,21 +11,20 @@ const TrackerItems = ({job, index}) => {
             {/*snapshot - contains # of property to style draggable components during drag */}
             {(provided, snapshot) => {
                 //Another approach to do the same thing by determine the bg color based on dragging state
-                const bgColor = snapshot.isDragging ? 'bg-orange-400' : 'bg-gray-400';
+                const borderColor = snapshot.isDragging ? 'border-red-700' : 'border-lime-700';
                 return (
                     <div {...provided.draggableProps}
                     ref = {provided.innerRef}
                     // isDragging={snapshot.isDragging}
                     // We dont use the above line due to that tailwind css doesn't support dynamic css properties
                     // Need to handle dynamic class names within the component logic
-                    className={`bg-white rounded-md p-2 mb-2 flex flex-nowrap`}>
-                        <div {...provided.dragHandleProps} 
-                            className={`w-10 h-10 mr-1 rounded-sm p-3 text-xl my-auto ${bgColor}`}></div>
-                        <div className='flex flex-col'>
-                            <h1 className='font-bold'>{job.title}</h1>
-                            <p>{job.company_name}</p>
-                            <p>{job.salary_range}</p>
+                    className={`bg-white rounded-md p-2 mb-2 border-l-4 ${borderColor}`}>
+                        <div {...provided.dragHandleProps} className='flex flex-col'>
+                            <h1 className='font-bold'>{job.company_name}</h1>
+                            <p className='bg-green-800 text-white px-2 rounded-sm text-sm w-fit my-1'>{job.title}</p>
+                            <p className='text-sm'>${job.salary}</p>
                         </div>
+                        <div className='flex justify-end'><button className='bg-gray-600 text-white px-2 rounded-sm text-sm w-fit cursor-pointer hover:bg-gray-900'>View</button></div>
                     </div>
                 )
             }}
