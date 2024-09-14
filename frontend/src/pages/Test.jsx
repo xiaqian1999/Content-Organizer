23f80c077726578abaa19ebbbaaad16f18484a9a
RoniaZhang <XiaQian1999@gmail.com>
2024-09-13 21:31:43 -0400

    Frontend: Clear the test component code

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,60 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import TrackerTransformData from '../components/TrackerTransformData';
+import React from 'react'
 
-const transformData = (data) => {
-  const jobs = {};
-  const columns = {
-    'column-1': {
-        id: 'column-1',
-        title: 'New',
-        jobIds: []
-    },
-    'column-2': {
-        id: 'column-2',
-        title: 'In Progress',
-        jobIds: []
-    },
-    'column-3': {
-        id: 'column-3',
-        title: 'Done',
-        jobIds: []
-    }
-  };
+const Test = () => {
+  return (
+    <div>Test</div>
+  )
+}
 
-  data.forEach(job => {
-      jobs[job._id] = { id: job._id, title: job.title };
-      columns['column-1'].jobIds.push(job._id);
-  });
-
-  return { jobs, columns };
-};
-
-const Test = ({url}) => {
-    const [data, setData] = useState(null);
-
-    const fetchList = async () => {
-        try {
-            const response = await axios.get(`${url}/api/post/list`);
-            const transformedData = transformData(response.data.data);
-            // console.log(transformedData);
-            setData(transformedData);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchList();
-    }, []);
-
-    return (
-        <div>
-            {data && (
-                <TrackerTransformData data={data} />
-            )}
-        </div>
-    );
-};
-
-export default Test;
+export default Test
