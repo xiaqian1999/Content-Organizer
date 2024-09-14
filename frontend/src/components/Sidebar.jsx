adfc0a8c0529b37fe50565f6bfc92111d7d7b889
RoniaZhang <XiaQian1999@gmail.com>
2024-09-13 21:56:33 -0400

    Frontend: selected sidebar with active design

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from "react-router-dom"
 import { assets } from '../assets/assets'
 
 const Sidebar = () => {
-  return (
-    <div className='font-semibold text-white bg-green-700 m-5 rounded-lg md:block xs:hidden' style={{height: '100vh', minWidth: '200px', maxWidth: '200px'}}>
-        <div>
-            <img src={assets.logo_white} alt="" />
+    const [active, setActive] = useState("dashboard")
+    return (
+        <div className='font-semibold text-white bg-green-700 m-5 rounded-lg md:block xs:hidden' style={{height: '100vh', minWidth: '200px', maxWidth: '200px'}}>
+            <div>
+                <img src={assets.logo_white} alt="" />
+            </div>
+            <div className='flex flex-col'>
+                <NavLink to={"/"} 
+                    onClick={() => {setActive("dashboard")}} 
+                    className={`flex items-center gap-4 mb-2 px-3 rounded-l-md cursor-pointer}`}>
+                    <p className={`hidden sm:block p-2 rounded-md w-full ${active==="dashboard" ? "bg-green-800" : ""}`}>Dashboard</p>
+                </NavLink>
+                <NavLink to={"/listjob"} 
+                    onClick={() => {setActive("listjob")}} 
+                    className={`flex items-center gap-4 mb-2 px-3 rounded-l-md cursor-pointer}`}>
+                    <p className={`hidden sm:block p-2 rounded-md w-full ${active==="listjob" ? "bg-green-800" : ""}`}>List Job</p>
+                </NavLink>
+                <NavLink to={"/jobtracker"} 
+                    onClick={() => {setActive("jobtracker")}} 
+                    className={`flex items-center gap-4 mb-2 px-3 rounded-l-md cursor-pointer}`}>
+                    <p className={`hidden sm:block p-2 rounded-md w-full ${active==="jobtracker" ? "bg-green-800" : ""}`}>Job Tracker</p>
+                </NavLink>
+                <NavLink to={"/test"} 
+                    onClick={() => {setActive("test")}} 
+                    className={`flex items-center gap-4 mb-2 px-3 rounded-l-md cursor-pointer}`}>
+                    <p className={`hidden sm:block p-2 rounded-md w-full ${active==="test" ? "bg-green-800" : ""}`}>Test Page</p>
+                </NavLink>
+            </div>
         </div>
-        <div className='pt-5 pl-[20%] flex flex-col'>
-            <NavLink to={"/"} className="flex items-center gap-4 py-5 px-3 rounded-l-md cursor-pointer">
-                <p className='hidden sm:block'>Dashboard</p>
-            </NavLink>
-            <NavLink to={"/listjob"} className="flex items-center gap-4 py-5 px-3 rounded-l-md cursor-pointer">
-                <p className='hidden sm:block'>List Job</p>
-            </NavLink>
-            <NavLink to={"/jobtracker"} className="flex items-center gap-4 py-5 px-3 rounded-l-md cursor-pointer">
-                <p className='hidden sm:block'>Job Tracker</p>
-            </NavLink>
-            <NavLink to={"/test"} className="flex items-center gap-4 py-5 px-3 rounded-l-md cursor-pointer">
-                <p className='hidden sm:block'>Test Page</p>
-            </NavLink>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
